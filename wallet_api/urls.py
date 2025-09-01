3dcc860434c4f72fbbe486f9248023b5d9430f3d
moataz <moatazbanmansour@gmail.com>
2025-09-01 10:42:14 +0100

    New files has been added

--- wallet_api/urls.py
+++ wallet_api/urls.py
@@ -16,26 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Wallet API",
-        default_version='v1',
-        description="API documentation for Wallet Project",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("wallet.urls")),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
